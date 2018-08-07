34f645eedbc2a61781ab3c21cca22073767ae37e
Ting <ticccccc@163.com>
2018-08-07 09:18:18 +0800

    添加时间转换的Util

--- app/src/main/java/com/dexin/testuser/NearAdapter.kt
+++ app/src/main/java/com/dexin/testuser/NearAdapter.kt
@@ -3,6 +3,9 @@ package com.dexin.testuser
 import com.amap.api.services.nearby.NearbyInfo
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
+import com.dexin.testuser.common.util.TimeUtils
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 /**
  * @author Ting
@@ -13,10 +16,12 @@ class NearAdapter(val resLayout: Int, val list: MutableList<NearbyInfo>)
     override fun convert(helper: BaseViewHolder?, item: NearbyInfo?) {
         if (helper != null) {
             if (item != null) {
+                // 格式：年－月－日 小时：分钟：秒
+
                 helper.setText(R.id.mUserId, "用户ID=${item.userID}")
                 helper.setText(R.id.mDistanceTv, "周围距离=${item.distance}")
                 helper.setText(R.id.mDriverDistanceTv, "周围驾驶=${item.drivingDistance}")
-                helper.setText(R.id.mTimeTv, "周围用户时间=${item.timeStamp}")
+                helper.setText(R.id.mTimeTv, "周围用户时间=${TimeUtils.secondToString(item.timeStamp)}")
                 helper.setText(R.id.mPointTv, "周围用户点=${item.point}")
             }
         }
